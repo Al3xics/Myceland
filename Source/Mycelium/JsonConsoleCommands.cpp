c23a0707920e9f79d481568f532ba27a521b3564
Vulji <valentin.luzolo@protonmail.com>
2026-01-12 21:19:10 +0100

    Added load test Json

--- Source/Mycelium/JsonConsoleCommands.cpp
+++ Source/Mycelium/JsonConsoleCommands.cpp
@@ -3,22 +3,52 @@
 #include "Serialization/JsonSerializer.h"
 
 // Global-scope static console command
-static FAutoConsoleCommand TestJsonCmd(
-	TEXT("Test.Json"),
-	TEXT("Tests JSON save/load"),
+static FAutoConsoleCommand TestSaveCmd(
+	TEXT("Test.Save"),
+	TEXT("Tests JSON save"),
 	FConsoleCommandDelegate::CreateStatic([]()
 	{
 		JsonSaveSystem SaveSystem;
 		FString Path = FPaths::ProjectSavedDir() / TEXT("ConsoleTest.json");
 
 		TSharedPtr<FJsonObject> Obj = MakeShared<FJsonObject>();
-		Obj->SetStringField("Level", "01");
-		Obj->SetStringField("PlayerName", "Maman");
-		Obj->SetStringField("MusicVolume", "50");
+		Obj->SetNumberField("Level", 8);
+		Obj->SetStringField("PlayerName", "Babu");
+		Obj->SetNumberField("MusicVolume", 25);
 		Obj->SetStringField("Resolution", "1920*1080");
 
 		SaveSystem.WriteJsonFile(Path, Obj);
 
-		UE_LOG(LogTemp, Warning, TEXT("Test.Json executed"));
+		UE_LOG(LogTemp, Warning, TEXT("Test.Save executed"));
+	})
+);
+static FAutoConsoleCommand TestLoadCmd(
+	TEXT("Test.Load"),
+	TEXT("Tests JSON load"),
+	FConsoleCommandDelegate::CreateStatic([]()
+	{
+		JsonSaveSystem SaveSystem;
+		FString Path = FPaths::ProjectSavedDir() / TEXT("ConsoleTest.json");
+
+		TSharedPtr<FJsonObject> LoadedJson = SaveSystem.ReadJsonFile(Path);
+
+		if (LoadedJson.IsValid())
+		{
+			FString PlayerName;
+			int32 Music = 0, Level = 0;
+			FString Resolution;
+
+			LoadedJson->TryGetStringField(TEXT("PlayerName"), PlayerName);
+			LoadedJson->TryGetNumberField(TEXT("MusicVolume"),  Music); 
+			LoadedJson->TryGetNumberField(TEXT("Level"), Level); 
+			LoadedJson->TryGetStringField(TEXT("Resolution"), Resolution);
+
+
+			UE_LOG(LogTemp, Warning, TEXT("Loaded JSON: %s Level=%d Music=%d Res=%s"),
+			       *PlayerName, Level, Music, *Resolution);
+
+
+			UE_LOG(LogTemp, Warning, TEXT("Test.Load executed"));
+		}
 	})
 );
