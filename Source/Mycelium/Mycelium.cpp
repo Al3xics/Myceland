ef27239d199292f14578694ea0bd737809da7c15
Alexis <alexis.martinho.78@gmail.com>
2026-01-12 11:54:08 +0100

    First Commit

---
+++ Source/Mycelium/Mycelium.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Mycelium.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Mycelium, "Mycelium" );
+
+DEFINE_LOG_CATEGORY(LogMycelium)
+ 
---
+++ Source/Mycelium/MyceliumGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MyceliumGameMode.h"
+
+AMyceliumGameMode::AMyceliumGameMode()
+{
+	// stub
+}
