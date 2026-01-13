d2eb7e4e762a2fa118990f7cf5cacb3e3bc61b57
Phylip3 <martin.revolbuisson@gmail.com>
2026-01-13 10:18:16 +0100

    Add AMyceliumCase actor with collision setup and related assets

---
+++ Source/Mycelium/Public/MyceliumCase.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MyceliumCase.generated.h"
+
+UCLASS(Blueprintable)
+class MYCELIUM_API AMyceliumCase : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AMyceliumCase();
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Case")
+	UStaticMeshComponent* CaseMesh;
+};
