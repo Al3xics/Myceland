62250ec1e1a05a90372977a141d236ff8e1ededb
RalphKK05 <ralphkmeid2001@gmail.com>
2026-05-01 11:30:09 +0200

    WaterStateChange

--- Source/Myceland/Public/Tiles/TileBase/ML_TileWater.h
+++ Source/Myceland/Public/Tiles/TileBase/ML_TileWater.h
@@ -5,7 +5,12 @@
 #include "CoreMinimal.h"
 #include "Tiles/ML_TileBase.h"
 #include "ML_TileWater.generated.h"
-
+UENUM(BlueprintType)
+enum class EML_WaterState : uint8
+{
+	Dead  UMETA(DisplayName="Dead"),
+	Alive UMETA(DisplayName="Alive")
+};
 UCLASS()
 class MYCELAND_API AML_TileWater : public AML_TileBase
 {
@@ -14,6 +19,15 @@ class MYCELAND_API AML_TileWater : public AML_TileBase
 public:
 	// Sets default values for this actor's properties
 	AML_TileWater();
+	
+	UPROPERTY(BlueprintReadOnly, Category="Myceland Water")
+	EML_WaterState WaterState = EML_WaterState::Dead;
+
+	UFUNCTION(BlueprintCallable, Category="Myceland Water")
+	void SetWaterState(EML_WaterState NewState);
+
+	UFUNCTION(BlueprintImplementableEvent, Category="Myceland Water")
+	void OnWaterStateChanged(EML_WaterState NewState);
 
 protected:
 	// Called when the game starts or when spawned
